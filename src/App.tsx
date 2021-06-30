440e38826d6f32f845f3b291d7ccdccbe1c7f8fe
unknown <mhassantownsend@gmail.com>
2021-06-29 22:15:13 -0400

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  FormFeedback
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      validate: {
+        emailState: '',
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateEmail(e) {
+    const emailRex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const { validate } = this.state;
+
+    if (emailRex.test(e.target.value)) {
+      validate.emailState = 'has-success';
+    } else {
+      validate.emailState = 'has-danger';
+    }
+
+    this.setState({ validate });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${this.state.email}`);
+  }
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="App">
+        <h2>Illumitwitter</h2>
+        <Form className="form" onSubmit={(e) => this.submitForm(e)}>
+          <FormGroup>
+            <Label for="exampleEmail">Username</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="example@example.com"
+              valid={this.state.validate.emailState === "has-success"}
+              invalid={this.state.validate.emailState === "has-danger"}
+              value={email}
+              onChange={(e) => {
+                this.validateEmail(e);
+                this.handleChange(e);
+              }}
+            />
+            
+            
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="********"
+            />
+          </FormGroup>
+        <Button id="submit-button">Submit</Button>
+      </Form>
     </div>
   );
 }
+}
 
 export default App;
+
+
+
