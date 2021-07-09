f29f8f836355b6fed4e1fbb7c0280cc9a79596db
Kai <kaiyip.ho23@gmail.com>
2021-07-09 05:40:02 -0400

    card feed style

--- src/Components/Post.tsx
+++ src/Components/Post.tsx
@@ -39,11 +39,12 @@ const Post: React.FC<IProps> = (props: IProps) => {
                     <CardImg id="card-image" top width="10px" height="80px" src={props.post.displayImg} alt={props.post.displayImg} />
                     <CardTitle style={{ cursor: 'pointer' }} id="card-title" tag="h5" onClick={(e) => setRedirect("/" + props.post.userName)}>{props.post.displayName}</CardTitle>
                     <CardSubtitle id="card-subtitle" tag="h6" className="mb-2 text-muted">@{props.post.userName}</CardSubtitle>
+                    </CardBody>
                     <CardText id="body">{props.post.postBody}</CardText>
                     {props.post.postImg && <CardImg src={props.post.postImg} alt={props.post.postImg} />}
                     <CardText id="timestamp">{new Date(props.post.postTime || 0).toLocaleString()}</CardText>
                     {(user?.userName === props.post.userName) && <Button onClick={() => deletePost(user.userName, props.post.postTime || 0)}>Delete</Button>}
-                </CardBody>
+               
             </Card>
         </div>
     );
