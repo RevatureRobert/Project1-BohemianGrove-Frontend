214069e62d9e83ecc4d61f0b610c5813b4ce2bda
Samuel Smetzer <kamuela.1300@gmail.com>
2021-07-09 09:35:48 -0700

    13 tests done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,239 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import SignInComponent from './Components/SignIn';
-import SignUpComponent from './Components/SignUp';
+import Login from  './Screens/Login';
+import SignUp from './Screens/SignUp';
 import NavigationBar from './Components/NavBar';
-import SettingsComponent from './Components/Profile';
-import ProfileComponent from './Components/Profile';
-import CardContainer from './Components/Card';
+import Settings from './Screens/Settings';
+import SettingsComponent from './Components/Settings';
+import Profile from './Screens/Profile';
 import Feed from './Components/Feed';
-import Router from 'react-router';
+import GlobalFeed from './Screens/GlobalFeed';
+import Splash from './Screens/Splash';
+import Search from './Screens/Search';
+import Router from 'react-router-dom';
 import App from './App';
-import shallow from '@wojtekmaj/enzyme-adapter-react-17';
-import { createMemoryHistory } from "history";
-import { reducer } from './Redux/Store'
-import { initialState } from './Redux/State';
-import { mount } from 'enzyme';
+import { initialState } from './Redux/Store';
+import { mount, shallow } from 'enzyme';
+import  Adapter  from '@wojtekmaj/enzyme-adapter-react-17'
+import enzyme from 'enzyme'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Reducer } from 'react'
+import { reducers } from './Redux/Reducers';
+import CreatePost from './Components/CreatePost';
+import Post from './Components/Post';
+import UserList from './Components/UserList';
+import User from './Components/User'
 
+
+
+const store = createStore(reducers);
+
+enzyme.configure({ adapter: new Adapter() });
+
+describe('SignUpComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<MemoryRouter><SignUp/></MemoryRouter>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('SignInUpComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<MemoryRouter><Login/></MemoryRouter>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('SettingsComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = mount(<Provider store={store}><MemoryRouter><Settings/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('GlobalFeedComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><GlobalFeed/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('ProfileComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><Profile/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('SplashComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Splash/>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('SearchComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><Search/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('App', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<App/>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('CreatePostComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><CreatePost/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('PostComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><Post post = {{userName:'',displayName:'',displayImg:''}} /></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('SettingsComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><SettingsComponent/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('UserComponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<User/>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('UserListCoponent', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<UserList users = {[]}/>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+/*
+describe('NavigationBar', ()=>{
+  it('renders without crashing', () => {
+  const component = shallow(<Provider store={store}><MemoryRouter><NavigationBar/></MemoryRouter></Provider>);
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+describe('Feed', ()=>{
+  it('renders without crashing', () => {
+    
+  const component = shallow(<Feed posts = {[]}/>);
+
+  expect(component).toMatchSnapshot();  
+  });
+})
+
+
+/*
+
+
+  it("should render title", () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <SignUpComponent />
+     </MemoryRouter>
+    );
+    expect(wrapper.queryAllByText("Bohemian Grove")).toHaveLength(1);
+  });
+
+  it("should render title", () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <SignInComponent />
+     </MemoryRouter>
+    );
+    expect(wrapper.queryAllByText("Bohemian Grove")).toHaveLength(1);
+  });
+
+
+it('has a languageName of "TypeScript"', () => {
+  const state: typeof initialState = { 
+    
+    user: {
+        userName: ' ',
+        displayName: ' ',
+        email: ' ',
+        profileImg: 'png',
+        loginToken: 'TypeScript',
+    }
+  };
+  expect(state.user?.loginToken).toEqual('TypeScript');
+});
+
+
+it('has a userName', () => {
+  const state: typeof initialState = { 
+    
+    user: {
+        userName: 'johncena',
+        displayName: ' ',
+        email: ' ',
+        profileImg: 'png',
+        loginToken: 'TypeScript',
+    }
+  };
+  expect(state.user?.userName).toEqual('johncena');
+});
+
+/*
+test('should return the initial state', () => {
+  expect(reducer(undefined, {})).toEqual(
+    {
+      loginToken: " ",
+      user:{
+        displayName: ' ',
+        email: ' ',
+        password: ' ',
+        profileImg:'png',
+        userName:' '
+        }
+    }
+  )
+})
+*/
+/*
+describe('manage profile page test suite', () => {
+  it('snapshot test', () => {
+      const setRouteLeaveHook =jest.fn();
+
+      let wrapper = shallow(
+          <Feed params={{router: 
+      setRouteLeaveHook}}/>
+      );
+    expect(wrapper).toMatchSnapshot();
+  })
+ })
+ 
+ it("should render title", () => {
+  const wrapper = render(
+    <MemoryRouter>
+      <Feed posts = {[]}/>
+    </MemoryRouter>
+    
+  );
+  expect(wrapper.queryAllByText("What's on your mind?")).toHaveLength(1);
+});
+
+
+/*
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><SignUpComponent /></MemoryRouter>, div);
@@ -180,3 +397,4 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CardContainer />, div);
 });
+*/
